dcace5c8fc7e07f47896eb146b65574473f223f1
Binu_Tamang <bunutmg323@gmail.com>
2025-05-02 01:13:39 +0545

    chore(project): add base folders for pages, assets and tailwind css setup

---
+++ login_page/src/App.jsx
@@ -0,0 +1,16 @@
+// import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Click Me
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
